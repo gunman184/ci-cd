5cec96d316cc87af34f81a0bf4623e813e7313e3
Marcel Sanchez <marcelsanchez@Marcels-MacBook-Air.local>
2025-04-09 18:13:33 -0400

    test1

--- tests/integration/test_api_processing.py
+++ tests/integration/test_api_processing.py
@@ -4,3 +4,9 @@ endpoint = 'https://5rx2ovxoi9.execute-api.us-east-1.amazonaws.com/production/ap
 def test_call_endpoint():
     response = requests.get(endpoint)
     assert response.status_code == 200
+
+def test_can_increase():
+    response = requests.post(endpoint)
+    assert response.status_code == 200
+    data  =response.json()
+    print(data)
